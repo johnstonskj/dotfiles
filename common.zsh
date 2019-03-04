967c44978747b5a247ffe1e48da2c18874ee44ca
Simon Johnston <johnstonskj@gmail.com>
2019-03-04 13:21:52 -0800

    iUpdated with config from MacOS

---
+++ common.zsh
@@ -0,0 +1,144 @@
+LOGLEVEL=1
+
+__log() {
+    printf "%s %8d %8s [%s] %s\n"  `date "+%Y:%m:%dT%H:%M:%S%Z"` $$ `logname` $1 $2
+}
+
+log() {
+    print $1 $2
+    if [ $1 -ge $LOGLEVEL ] ; then
+	case $1 in
+	    0)
+		__log "CRITICAL" $2
+		exit 1
+		;;
+	    1)
+		__log "ERROR" $2
+		;;
+	    2)
+		__log "WARNING" $2
+		;;
+	    3)
+		__log "INFO" $2
+		;;
+	    *)
+		__log "DEBUG" $2
+		;;
+	esac
+    fi
+}
+
+log-critical() { log 0 $@ }
+log-error() { log 1 $@ }
+log-warning() { log 2 $@ }
+log-info() { log 3 $@ }
+log-debug() { log 4 $@ }
+
+create_development_dir() {
+    if [[ $OSTYPE = (darwin|freebsd)* ]] ; then
+	export DEVHOME=$HOME/Projects
+    else
+	export DEVHOME=$HOME/development
+    fi
+    if [ ! -d "$DEVHOME" ] ; then
+	mkdir $DEVHOME
+    fi
+}
+
+install_package_manager() {
+    log-debug "installing package manager"
+    if [[ $OSTYPE = (darwin|freebsd)* ]] ; then
+	if [ ! -d "/usr/local/Homebrew" ]; then
+	    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+    fi
+}
+
+update_package_manager() {
+    log-debug "updating package manager"
+    if [[ $OSTYPE = (darwin|freebsd)* ]] ; then
+	brew update
+    else
+	sudo apt update
+	apt list --upgradable
+    fi
+}
+
+install_package_for() {
+    if [[ $OSTYPE = $1* ]] ; then
+	shift
+	install_package $@
+    fi
+}
+
+install_package() {
+    if [[ $OSTYPE = (darwin|freebsd)* ]] ; then
+	brew install $@
+    else
+	sudo apt install $@
+    fi
+}
+
+update_package() {
+    if [[ $OSTYPE = (darwin|freebsd)* ]] ; then
+	brew upgrade $@
+    else
+	sudo apt upgrade $@
+    fi
+}
+
+link_dot_file() {
+    ln -s $DOTFILEDIR/$1 $2
+}
+
+install_zsh() {
+    log-debug "++ zsh"
+    install_package zsh
+    sudo chsh -s `which zsh`
+    link_dot_file dot-zshrc $HOME/.zshrc
+    link_dot_file dot-zshenv $HOME/.zshenv
+    
+    log-debug "++ oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    export ZSH=~/.oh-my-zsh
+
+    log-debug "++ oh-my-zsh plugins"
+    git clone https://github.com/bhilburn/powerlevel9k.git $ZSH/custom/themes/powerlevel9k
+    git clone https://github.com/zsh-users/zsh-docker.git $ZSH/custom/plugins/zsh-docker
+
+    log-debug "++ powerline fonts"
+    if [[ $OSTYPE = (darwin|freebsd)* ]] ; then
+	install_package homebrew/cask-fonts/font-meslo-nerd-font
+	install_package homebrew/cask-fonts/font-meslo-nerd-font-mono
+    else
+	install_package fonts-powerline
+    fi
+}
+
+update_zsh() {
+    update_package zsh
+    upgrade_oh_my_zsh
+}
+
+install_nvidia_cuda() {
+    if [[ $OSTYPE = (darwin|freebsd)* ]] ; then
+	log-warning "Unsupported under MacOS"
+    else
+	log-debug "++ graphics drivers"
+	local NVVER=`nvidia-smi |grep Version |awk '{ print $6 }'`
+	if [ ! "$NVVER"  = "418.43" ]; then
+	    curl -o ~/Downloads/NVIDIA-Linux-x86_64-418.43.run "http://us.download.nvidia.com/XFree86/Linux-x86_64/418.43/NVIDIA-Linux-x86_64-418.43.run"
+	    sudo sh NVIDIA-Linux-x86_64-418.43.run
+	    log-debug "++ turning off Nouveau X drivers"
+	    sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+	    sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+	fi
+	log-info "++ reboot now for driver update"
+
+	log-debug "++ CUDA programming support..."
+	curl -o ~/Downloads/cuda_10.1.105_418.39_linux.run "https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.105_418.39_linux.run"
+	sudo sh cuda_10.1.105_418.39_linux.run
+	mkdir /media/simon/development/cuda
+	cuda-install-samples-10.1.sh /media/simon/development/cuda/
+    fi
+}
--- setup.zsh
+++ setup.zsh
@@ -1,62 +1,33 @@
 #! /usr/bin/env zsh
 
-export DOTFILES=${0:a:h}
-export DEVHOME=~/development
+DOTFILEDIR=${0:a:h}
 
-if [ ! -d "$DEVHOME" ] ; then
-    mkdir $DEVHOME
-fi
+create_development_dir
 
-echo Update packages...
-sudo apt update
-apt list --upgradable
+install_package_manager
+update_package_manager
 
-# ********** tools **********
-echo System tools...
-sudo apt install tmux glances emacs-nox
-# https://github.com/tmuxinator/tmuxinator
-sudo apt install tmuxinator
+log-info "System tools..."
+install_package docker parallel htop glances tmux
+install_package_for linux emacs-nox
+install_package_for darwin emacs markdown-mode
 
-# ********** tools **********
-sudo apt install git
-ln -s $DOTFILES/dot-gitconfig ~/.gitconfig
-ln -s $DOTFILES/dot-gitignore_global ~/.gitignore_global
+log-info "Git"
+install_package git git-lfs
+link_dot_file dot-gitconfig $HOME/.gitconfig
+link_dot_file dot-gitignore_global $HOME/.gitignore_global
 
-# ********** zsh **********
-echo zsh...
-sudo apt-get install zsh autojump fonts-powerline
-sudo chsh -s `which zsh`
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-export ZSH=~/.oh-my-zsh
-git clone https://github.com/bhilburn/powerlevel9k.git $ZSH/custom/themes/powerlevel9k
-ln -s ~/.devenv/dot-zshrc ~/.zshrc
+log-info "Zshell..."
+install_zsh
 
-cd $DEVHOME
-git clone https://github.com/ryanoasis/nerd-fonts.git
-cd nerd-fonts.git
-./install.sh --copy --install-to-user-path --otf --complete
+log-info "Programming languages..."
+install_package_for linux gcc
+install_package_for darwin kotlin
+install_package anaconda racket
+# curl -sSL https://get.rvm.io | bash -s stable
 
-# ********** languages **********
-echo Programming languages...
-sudo apt install gcc racket
-sudo apt install anaconda
+log-info "NVIDIA CUDA..."
+install_nvidia_cuda
 
-# ********** cuda **********
-echo NVIDIA CUDA...
-nvidia-smi |grep Version
-NVVER=`nvidia-smi |grep Version |awk '{ print $6 }'`
-if [ ! "$NVVER"  = "418.43" ]; then
-    curl -o ~/Downloads/NVIDIA-Linux-x86_64-418.43.run "http://us.download.nvidia.com/XFree86/Linux-x86_64/418.43/NVIDIA-Linux-x86_64-418.43.run"
-    sudo sh NVIDIA-Linux-x86_64-418.43.run
-    sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-    sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-fi
-echo (reboot now for driver update)
-
-echo CUDA Programming support...
-curl -o ~/Downloads/cuda_10.1.105_418.39_linux.run "https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.105_418.39_linux.run"
-sudo sh cuda_10.1.105_418.39_linux.run
-mkdir /media/simon/development/cuda
-cuda-install-samples-10.1.sh /media/simon/development/cuda/
-
+log-info "Machine Leaning..."
 conda install pytorch torchvision cudatoolkit=10.0 -c pytorch

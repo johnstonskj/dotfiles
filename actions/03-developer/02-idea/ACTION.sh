9fe6c546fcec674e140d128cbe7a3613a2f36707
Simon Johnston <johnstonskj@gmail.com>
2020-12-22 09:49:09 -0800

    Log the settings repo URL

--- actions/03-developer/02-idea/ACTION.sh
+++ actions/03-developer/02-idea/ACTION.sh
@@ -1,2 +1,4 @@
 install_package_for linux -app intellij-idea-community --classic
 install_package_for macos -app intellij-idea-ce
+
+log-info "Settings repository URL: https://github.com/johnstonskj/idea-settings.git"

0c20528a66e36a0a4e446a369b51d9378faa3bc8
Simon Johnston <johnstonskj@gmail.com>
2020-12-23 09:12:43 -0800

    fixed issues with bat config

--- actions/01-os/05-bat-conf/ACTION.sh
+++ actions/01-os/05-bat-conf/ACTION.sh
@@ -1,2 +1,7 @@
+
 run_command mkdir -p $LOCAL_CONFIG/bat
-link_file bat.conf $LOCAL_CONFIG/bat/bat.conf
+link_file config $LOCAL_CONFIG/bat/config
+
+if [[ $ACTION = update ]] ; then
+	run_command bat cache --build
+fi

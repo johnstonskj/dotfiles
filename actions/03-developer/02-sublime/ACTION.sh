ea22538f76570af0531eb218a6a2070fd9baf304
Simon Johnston <johnstonskj@gmail.com>
2021-01-18 09:37:30 -0800

    add link to sublime install

--- actions/03-developer/02-sublime/ACTION.sh
+++ actions/03-developer/02-sublime/ACTION.sh
@@ -1,2 +1,6 @@
 install_package_for macos sublime-text
 install_package_for macos sublime-merge
+
+if [[ $ACTION = (install|update|link) ]] ; then
+    run_command ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl $LOCAL_BIN/subl
+fi

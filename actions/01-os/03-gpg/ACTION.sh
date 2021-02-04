12e3fbf18a60641a6fc60596001f4be1f1926732
Simon Johnston <johnstonskj@gmail.com>
2021-02-04 08:16:57 -0800

    Adding GnuPG configuration for IDE signing commits

--- actions/01-os/03-gpg/ACTION.sh
+++ actions/01-os/03-gpg/ACTION.sh
@@ -1,7 +1,13 @@
 install_package gpg
+install_package_for macos pinentry-mac
 
-if [[ $ACTION = (install|update) ]] ; then
-	if [ ! -d $HOME/.gnupg ] ; then
+GPG_HOME=$HOME/.gnupg
+
+link_file gpg-agent.conf $GPG_HOME/gpg-agent.conf
+link_file gpg.conf $GPG_HOME/gpg.conf
+
+if [[ $ACTION = install ]] ; then
+	if [ ! -d $GPG_HOME ] ; then
 	    log-info "++ GPG initialization..."
 	    run_command gpg --list-keys
 	fi

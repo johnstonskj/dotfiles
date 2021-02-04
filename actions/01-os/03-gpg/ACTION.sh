b1c163e143d0dc29d704f717e55f382ba5330fe3
Simon Johnston <johnstonskj@gmail.com>
2021-02-04 08:32:36 -0800

    Added gnupg agent conf for mac and linux

--- actions/01-os/03-gpg/ACTION.sh
+++ actions/01-os/03-gpg/ACTION.sh
@@ -4,8 +4,13 @@ install_package_for macos pinentry-mac
 
 GPG_HOME=$HOME/.gnupg
 
-link_file gpg-agent.conf $GPG_HOME/gpg-agent.conf
 link_file gpg.conf $GPG_HOME/gpg.conf
+
+if [[ $OPSYS = linux ]] ; then
+	link_file gpg-agent-linux.conf $GPG_HOME/gpg-agent.conf
+if [[ $OPSYS = macos ]] ; then
+	link_file gpg-agent-mac.conf $GPG_HOME/gpg-agent.conf
+fi
 
 if [[ $ACTION = install ]] ; then
 	if [ ! -d $GPG_HOME ] ; then

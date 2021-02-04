3ec3187c399888c77973188026066b7b925a5449
Simon Johnston <johnstonskj@gmail.com>
2021-02-04 08:26:42 -0800

    Fixed bug in link creation

--- actions/01-os/04-tools/ACTION.sh
+++ actions/01-os/04-tools/ACTION.sh
@@ -5,6 +5,6 @@ install_package_for macos coreutils gnu-sed
 install_package_for linux fd-find
 install_package_for macos fd
 
-link_file credback $LOCAL_BIN/ddcpimg
+link_file credback $LOCAL_BIN/credback
 link_file ddcpimg $LOCAL_BIN/ddcpimg
 link_file lcolor $LOCAL_BIN/lcolor

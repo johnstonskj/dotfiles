c8b96d77d73b979a171293c43ffe90ada18bebec
Simon Johnston <johnstonskj@gmail.com>
2021-02-24 08:53:24 -0800

    Added Libertine fonts

--- actions/01-os/02-fonts/ACTION.sh
+++ actions/01-os/02-fonts/ACTION.sh
@@ -3,3 +3,5 @@ install_package_for macos -app homebrew/cask-fonts/font-fira-code-nerd-font
 install_package_for macos -app font-meslo-lg
 install_package_for macos -app font-meslo-lg-nerd-font
 install_package_for linux fonts-powerline
+
+install_package_for macos -app font-linux-libertine

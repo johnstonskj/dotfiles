c88ccb6dc36fb4382a931d9b748e72ac7e92c669
Simon Johnston <johnstonskj@gmail.com>
2022-06-21 08:04:59 -0700

    Add git add-status command

--- actions/03-developer/01-git/ACTION.sh
+++ actions/03-developer/01-git/ACTION.sh
@@ -8,5 +8,6 @@ link_file ignore_global $GIT_CONFIG/ignore_global
 link_file message $GIT_CONFIG/message
 make_dir $GIT_CONFIG/hooks
 
+link_file git-all-status $LOCAL_BIN/git-all-status
 link_file git-tag-delete $LOCAL_BIN/git-tag-delete
 link_file git-tag-replace $LOCAL_BIN/git-tag-replace

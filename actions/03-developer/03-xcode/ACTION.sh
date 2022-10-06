339f430a98bd7d69f68922491f925c1337385814
Simon Johnston <johnstonskj@gmail.com>
2022-10-06 11:03:12 -0700

    Updated xcode action for full install

--- actions/03-developer/03-xcode/ACTION.sh
+++ actions/03-developer/03-xcode/ACTION.sh
@@ -1,4 +1,14 @@
 if [[ $OSSYS = macos && $ACTION = install ]] ; then
     xcode-select --install
-    sudo xcode-select --switch /Applications/Xcode.app
+    if [[ -d /Applications/Xcode.app ]]; then
+        sudo xcode-select --switch /Applications/Xcode.app
+    fi
 fi
+
+XCODE_THEMES=$(xcode-select -p)/UserData/FontAndColorThemes
+if [[ -d $XCODE_THEMES ]]; then
+    curl https://raw.githubusercontent.com/stackia/solarized-xcode/master/Solarized%20Dark.xccolortheme >"$XCODE_THEMES/Solarized Dark.xcolortheme"
+    curl https://raw.githubusercontent.com/stackia/solarized-xcode/master/Solarized%20Light.xccolortheme >"$XCODE_THEMES/Solarized Light.xcolortheme"
+fi
+
+link_env_file xcode-env

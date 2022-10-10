895ee7550836ca95509adc59490ae6a652ae04f1
Simon Johnston <johnstonskj@gmail.com>
2022-10-10 08:59:48 -0700

    Move Kitty to homebrew

--- actions/02-shell/02-kitty/ACTION.sh
+++ actions/02-shell/02-kitty/ACTION.sh
@@ -1,4 +1,8 @@
-run_command "curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin"
+if [[ -n $BREW ]] ; then
+    install_package_for macos -app kitty
+else
+    run_command "curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin"
+fi
 
 link_file kitty.conf $LOCAL_CONFIG/kitty/kitty.conf
 
